d4dde9dfc6cdeb3a1611fe7d1a84587f43a543c6
Italo P. Barbosa <italo@ideaalize.online>
2020-07-15 01:56:39 -0300

    teste

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var cors = require('cors')
+var app = express()
 
 app.use(cors())
 
-
-var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 8080
